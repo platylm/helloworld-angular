b59f559ce981a5b894bd78b2d998d7a16b24a418
platylm <n_nareenart@kkumail.com>
2019-08-11 13:22:05 +0700

    [Edited]: เพิ่มการเรียก fake api ที่ src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'frontend';
+  results: string;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Making the HTTP Request
+    this.http
+     .get('http://localhost:3000/say').subscribe((data:any)=>{
+       this.results = data.message;
+     });
+  }
 }
