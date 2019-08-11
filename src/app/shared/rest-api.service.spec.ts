9aefe36be9ea002d08130c6fc9005af984d07f36
platylm <n_nareenart@kkumail.com>
2019-08-11 16:17:48 +0700

    [Edited]: เพิ่ม test spy เข้าไปที่ src/app/

--- src/app/shared/rest-api.service.spec.ts
+++ src/app/shared/rest-api.service.spec.ts
@@ -1,15 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RestApiService } from './rest-api.service';
+import { Message } from './message';
 
 describe('RestApiService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule],
-        providers: [RestApiService]
-  }));
+
+  let service: RestApiService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [RestApiService]
+    });
+    service = TestBed.get(RestApiService);
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: RestApiService = TestBed.get(RestApiService);
     expect(service).toBeTruthy();
+  });
+
+  it('Service testing with HTTP client', () => { //Test spy 
+    // Arrange
+    const response = {
+      message: 'Dummy'
+    };
+
+    // Act and assert
+    service.getMessage().subscribe((data: Message) => { //Async call back
+      expect(data.message).toEqual('Dummy');
+    });
+
+    // Assert
+    const request = httpTestingController.expectOne('http://localhost:4444/say'); //มี method ช่วยว่า url นี้ถูกเรียกหรือไม่
+    expect(request.request.method).toEqual('GET');
+    request.flush(response);
+    httpTestingController.verify();
   });
 });
